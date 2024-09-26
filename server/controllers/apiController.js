7156e715f99cf59d1aca220de0301c41ece1a499
DiegoMHB <dmhb17@gmail.com>
2024-09-26 16:12:35 +0200

    intro

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,11 +1,14 @@
-require('dotenv').config({path: '.env'});
+require('dotenv').config();
 
 exports.getAccommodation = async (req, res) => {
   try {
     const { lon, lat } = req.query;
-    const apiKey = process.env.GOOGLE_API_KEY;
+    console.log(lon, lat);
+    const apiKey = process.env.REACT_APP_GOOGLE_PLACES_API_KEY;
+    console.log(process.env)
     const response = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apiKey}&location=${lat},${lon}&radius=500&type=lodging`);
     const data = await response.json();
+    console.log(data)
     res.status(200).json(data)
   } catch (error) {
     console.log(error);
