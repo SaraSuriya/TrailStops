3294c945131b0b0675ab146d28ec4290c22c7b19
Sara <sara@inkmoon.net>
2024-09-26 14:33:42 +0100

    Fixed local issue with env and solved delete marker issue.

--- client/src/components/detailSummary/detailSummary.js
+++ client/src/components/detailSummary/detailSummary.js
@@ -5,6 +5,7 @@ function DetailSummary({ markers }) {
   if (!markers || Object.keys(markers).length === 0) {
     return <div style={{zIndex:"-100", position:"absolute", top:"1px"}}>No markers placed!</div>;
   }
+  // Checks if any markers are displayed on the screen.
 
   const sortedMarkers = Object.values(markers || {}).sort((a, b) => a.order - b.order);
   
--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,9 +1,10 @@
 require('dotenv').config({path: '.env'});
 
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
 exports.getAccommodation = async (req, res) => {
   try {
     const { lon, lat } = req.query;
-    const apiKey = process.env.GOOGLE_API_KEY;
     const response = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apiKey}&location=${lat},${lon}&radius=500&type=lodging`);
     const data = await response.json();
     res.status(200).json(data)
@@ -16,7 +17,6 @@ exports.getAccommodation = async (req, res) => {
 exports.getAccommodationPic = async (req, res) => {
   try {
     const { photo_reference } = req.query;
-    const apiKey = process.env.GOOGLE_API_KEY;
     const imageUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photo_reference}&key=${apiKey}`;
     
     const response = await fetch(imageUrl);
@@ -36,7 +36,6 @@ exports.getAccommodationPic = async (req, res) => {
 exports.getAccomodationDetails = async (req, res) => {
   try {
     const { place_id } = req.query;
-    const apiKey = process.env.GOOGLE_API_KEY;
     const response = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?key=${apiKey}&place_id=${place_id}`);
     const data = await response.json();
     res.status(200).json(data)
